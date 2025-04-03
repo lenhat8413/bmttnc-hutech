1796f6c725a67d517c16c260b3156f74b670835c
lenhat8413 <lenhat8413@gmail.com>
2025-04-03 10:57:57 +0700

    fix code

--- lab_05/img-hidden/encrypt.py
+++ lab_05/img-hidden/encrypt.py
@@ -1,18 +1,42 @@
 import os
+import sys
 from PIL import Image
 
 def encode_image(image_path, message):
     image = Image.open(image_path)
     width, height = image.size
-    pixel_index =0
-    binary_message = '' .join(format(ord(char),'08b')for char in message)
-    binary_message += '1111111111111110'# End of message delimiter
+    binary_message = ''.join(format(ord(char), '08b') for char in message)
+    binary_message += '1111111111111110'  # End of message delimiter
 
     data_index = 0
     for row in range(height):
-        for col in range (width):
-            pixel =list (img.getpixel((col,row)))
+        for col in range(width):
+            pixel = list(image.getpixel((col, row)))
 
-            for color_channel
+            for color_channel in range(3):
+                if data_index < len(binary_message):
+                    pixel[color_channel] = int(format(pixel[color_channel], '08b')[:-1] + binary_message[data_index], 2)
+                    data_index += 1 
 
-    encoded_image.save('encoded_' + os.path.basename(imagepath))
+            image.putpixel((col, row), tuple(pixel))
+
+            if data_index >= len(binary_message):
+                break
+        if data_index >= len(binary_message):
+            break
+
+    encoded_image_path = 'encoded_image.png'
+    image.save(encoded_image_path)
+    print("Steganography complete! Encoded image saved as", encoded_image_path)
+
+def main():
+    if len(sys.argv) != 3:
+        print("Usage: python encode_image.py <image_path> <message>")
+        return
+    
+    image_path = sys.argv[1]
+    message = sys.argv[2]
+    encode_image(image_path, message)
+
+if __name__ == "__main__":
+    main()
